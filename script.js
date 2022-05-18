30ada7f80c01419f88ae853e7f1907e030b8e083
yagrand3z <49150955+yagrand3z@users.noreply.github.com>
2022-05-18 13:28:33 +0300

    update

--- script.js
+++ script.js
@@ -1,119 +1,120 @@
-class SetsCalc {
+class ActivityCalc {
 	constructor(){
-		this.rows = [];
-		this.table = $('#table');
-		this.row_tpl = $('#row_tpl');
-		this.summary_fields = [
-			$('#summary .item_component'),
-			$('#summary .item_bulb'),
-			$('#summary .item_coil'),
-			$('#summary .item_gear'),
-			$('#summary .item_brotonium'),
-			$('#summary .item_blueprints'),
-			$('#summary .item_hours_premium'),
-			$('#summary .item_hours'),
-		];
-		this.add_button = $('#add_button');
-		this.add_button.click(this.addRow.bind(this));
-		this.share_modal = $('#share_modal');
-		this.share_modal_input = $('#share_modal_input');
-		this.share_button = $('#share_button');
-		this.share_button.click(this.share.bind(this));
-		this.add_button.prop('disabled', true);
+		this.f = {
+			throne_room_level: $('#calc_throne_room_level'),
+			food_per_hour: $('#calc_food_per_hour'),
+			food_per_day: $('#calc_food_per_day'),
+			season_length: $('#calc_season_length'),
+			arena_raid_per_day: $('#calc_arena_raid_per_day'),
+			arena_avg_medals_per_raid: $('#calc_arena_avg_medals_per_raid'),
+			devourer_raid_per_day: $('#calc_devourer_raid_per_day'),
+			devourer_avg_eyes_per_raid: $('#calc_devourer_avg_eyes_per_raid'),
+			food_spending_balance_alert: $('#calc_food_spending_balance_alert'),
+		};
+		this.f.food_per_hour.change(this.onFoodPerHourChange.bind(this));
+		this.f.throne_room_level.change(this.calc.bind(this));
+		this.f.season_length.change(this.calc.bind(this));
+		this.f.arena_raid_per_day.change(this.calc.bind(this));
+		this.f.arena_avg_medals_per_raid.change(this.calc.bind(this));
+		this.f.devourer_raid_per_day.change(this.calc.bind(this));
+		this.f.devourer_avg_eyes_per_raid.change(this.calc.bind(this));
+		this.r = {
+			free_progressbar: $('#calc_free_progressbar'),
+			arena_progressbar: $('#calc_arena_progressbar'),
+			arena_food_spending: $('#calc_arena_food_spending'),
+			arena_food_cost_per_day: $('#calc_arena_food_cost_per_day'),
+			arena_food_cost_per_season: $('#calc_arena_food_cost_per_season'),
+			arena_medals_reward_per_day: $('#calc_arena_medals_reward_per_day'),
+			arena_medals_reward_per_season: $('#calc_arena_medals_reward_per_season'),
+			devourer_progressbar: $('#calc_devourer_progressbar'),
+			devourer_food_spending: $('#calc_devourer_food_spending'),
+			devourer_food_cost_per_day: $('#calc_devourer_food_cost_per_day'),
+			devourer_food_cost_per_season: $('#calc_devourer_food_cost_per_season'),
+			devourer_eyes_reward_per_day: $('#calc_devourer_eyes_reward_per_day'),
+			devourer_eyes_reward_per_season: $('#calc_devourer_eyes_reward_per_season'),
+		};
 		$.get('base.json', this.onBaseLoad.bind(this));
 	}
 	
-	getShareUrl(){
-		var url = window.location.origin+window.location.pathname,
-			data = [];
-		this.rows.forEach((row)=>{
-			data.push(row.url_data);
-		});
-		url += '#'+data.join('|');
-		return url;
-	}
-	
-	share(){
-		var url = this.getShareUrl();
-		SetsCalcHelper.copyTextToClipboard(url, this.onShareUrlCopied.bind(this));
-	}
-	
-	onShareUrlCopied(result){
-		if(result){
-			this.share_button.html('COPIED');
-			setTimeout(a=>{this.share_button.html('SHARE');}, 1500);
-		}else{
-			var url = this.getShareUrl();
-			this.share_modal_input.val(url);
-			this.share_modal.modal('show');
-		}
-	}
-	
-	formatTime(s){
-		 var fm = [
-			Math.floor(s / 60 / 60 / 24),
-			Math.floor(s / 60 / 60) % 24,
-			Math.floor(s / 60) % 60,
-			s % 60
-		],
-		map = ['d','h', 'm','s'],
-		str = '';
-		fm.forEach((v,k)=>{
-			if(v>0){
-				str+=v+map[k]+' ';
-			}
-		});
-      return str;
-	}
-	
-	addRow(){
-		this.rows.push(new SetsCalcRow(this));
+	onFoodPerHourChange(){
+		this.f.food_per_day.val(this.f.food_per_hour.val()*24);
+		this.calc();
 	}
 	
 	onBaseLoad(base){
 		this.base = base;
-		this.add_button.prop('disabled', false);
-		if(this.parseHash()){
-			this.applyHash();
-		}else{
-			this.add_button.click();
-		}
-	}
-	
-	parseHash(){
-		return window.location.hash.match(/(([a-z]+):(\d+):(\d+):(\d+))/g);
-	}
-	
-	applyHash(){
-		var matches = this.parseHash();
-		matches.forEach(str=>{
-			let row = new SetsCalcRow(this);
-			row.url_data = str;
-			this.rows.push(row);
-		});
 		this.calc();
 	}
 	
 	calc(){
-		var summary = [0,0,0,0,0,0,0,0];
-		this.rows.forEach((row)=>{
-			let row_summary = [0,0,0,0,0,0,0,0];
-			let results = this.base.filter(r=>r[0]>row.level_from&&r[0]<=row.level_to&&r[9]==row.type);
-			results.forEach(r=>{
-				row_summary[0]+=(r[1]);
-				row_summary[1]+=(r[2]);
-				row_summary[2]+=(r[3]);
-				row_summary[3]+=(r[4]);
-				row_summary[4]+=(r[5]);
-				row_summary[5]+=(r[6]);
-				row_summary[6]+=(r[7]);
-				row_summary[7]+=(r[8]);
-			});
-			row_summary.forEach((r,k)=>row_summary[k]*=row.quantity); 
-			row_summary.forEach((r,k)=>summary[k]+=r);
-			row.setCalc(row_summary);
-		});
-		this.setSummary(summary)
+		var arena = this.base.Arena[this.f.throne_room_level.val()],
+			arena_cost = arena.Food[0],
+			season_length = parseInt(this.f.season_length.val()),
+			arena_raid_per_day = parseInt(this.f.arena_raid_per_day.val()),
+			arena_avg_medals_per_raid = parseInt(this.f.arena_avg_medals_per_raid.val()),
+			food_per_day = parseInt(this.f.food_per_day.val()),
+			arena_food_cost_per_day = 0,
+			arena_food_cost_per_season = 0,
+			arena_medals_reward_per_day = 0,
+			arena_food_spending = 0,
+			arena_food_cost_per_season = 0;
+		for(var i=0; i<arena_raid_per_day;i++){
+			if(typeof arena.Food[i] == 'number')
+				arena_cost = arena.Food[i];
+			arena_food_cost_per_day += arena_cost;
+			arena_medals_reward_per_day += arena_avg_medals_per_raid;
+		}
+		arena_food_spending = Math.round(arena_food_cost_per_day/food_per_day*100, 2);
+		arena_food_spending = arena_food_spending>=0?arena_food_spending:0;
+		this.r.arena_food_spending.html(arena_food_spending+'%');
+		this.r.arena_progressbar.css({width:arena_food_spending+'%'});
+		this.r.arena_food_cost_per_day.html(arena_food_cost_per_day);
+		this.r.arena_food_cost_per_season.html(arena_food_cost_per_day*season_length);
+		this.r.arena_medals_reward_per_day.html(arena_medals_reward_per_day);
+		this.r.arena_medals_reward_per_season.html(arena_medals_reward_per_day*season_length);
+		
+		
+		var devourer = this.base.Devourer[this.f.throne_room_level.val()],
+			devourer_cost = devourer.Food[0],
+			season_length = parseInt(this.f.season_length.val()),
+			devourer_raid_per_day = parseInt(this.f.devourer_raid_per_day.val()),
+			devourer_avg_eyes_per_raid = parseInt(this.f.devourer_avg_eyes_per_raid.val()),
+			food_per_day = parseInt(this.f.food_per_day.val()),
+			devourer_food_cost_per_day = 0,
+			devourer_food_cost_per_season = 0,
+			devourer_eyes_reward_per_day = 0,
+			devourer_food_spending = 0,
+			devourer_food_cost_per_season = 0;
+		for(var i=0; i<devourer_raid_per_day;i++){
+			if(typeof devourer.Food[i] == 'number')
+				devourer_cost = devourer.Food[i];
+			devourer_food_cost_per_day += devourer_cost;
+			devourer_eyes_reward_per_day += devourer_avg_eyes_per_raid;
+		}
+		devourer_food_spending = Math.round(devourer_food_cost_per_day/food_per_day*100, 2);
+		devourer_food_spending = devourer_food_spending>=0?devourer_food_spending:0;
+		this.r.devourer_food_spending.html(devourer_food_spending+'%');
+		this.r.devourer_progressbar.css({width:devourer_food_spending+'%'});
+		this.r.devourer_food_cost_per_day.html(devourer_food_cost_per_day);
+		this.r.devourer_food_cost_per_season.html(devourer_food_cost_per_day*season_length);
+		this.r.devourer_eyes_reward_per_day.html(devourer_eyes_reward_per_day);
+		this.r.devourer_eyes_reward_per_season.html(devourer_eyes_reward_per_day*season_length);
+		
+		var summary_spending = arena_food_spending+devourer_food_spending,
+			summary_food_cost = devourer_food_cost_per_day+arena_food_cost_per_day;
+		if(summary_spending>100){
+			this.r.arena_food_spending.css({color:'red'});
+			this.r.devourer_food_spending.css({color:'red'});
+			this.f.food_spending_balance_alert.slideDown('fast');
+			this.r.free_progressbar.html('');
+			this.r.free_progressbar.css({width:'0%'});
+		}else{
+			this.r.free_progressbar.html(food_per_day-summary_food_cost);
+			this.r.free_progressbar.css({width:(100-summary_spending)+'%'});
+			this.r.arena_food_spending.css({color:'inherit'});
+			this.r.devourer_food_spending.css({color:'inherit'});
+			this.f.food_spending_balance_alert.slideUp('fast');
+		}
 	}
 	
 	setSummary(summary){
@@ -128,158 +129,6 @@ class SetsCalc {
 	
 }
 
-class SetsCalcRow {
-	constructor(calc, data){
-		this.calc = calc;
-		this.row = this.calc.row_tpl.clone();
-		this.calc.table.append(this.row);
-		this.row.removeClass('d-none');
-		this.row.attr({id:''});
-		this.type_dropdown = this.row.find('.item_type_dropdown');
-		this.type_dropdown.find('.dropdown-item').click(this.onTypeSelect.bind(this));
-		this.type_field = this.row.find('.item_type');
-		this.type_field.change(this.calc.calc.bind(this.calc));
-		this.quantity_field = this.row.find('.item_quantity');
-		this.quantity_field.change(this.calc.calc.bind(this.calc));
-		this.level_from_field = this.row.find('.item_level_from');
-		this.level_from_field.change(this.onFromChanged.bind(this));
-		this.level_to_field = this.row.find('.item_level_to');
-		this.level_to_field.change(this.onToChanged.bind(this));
-		this.summary_fields = [
-			this.row.find('.item_component'),
-			this.row.find('.item_bulb'),
-			this.row.find('.item_coil'),
-			this.row.find('.item_gear'),
-			this.row.find('.item_brotonium'),
-			this.row.find('.item_blueprints'),
-			this.row.find('.item_hours_premium'),
-			this.row.find('.item_hours'),
-		];
-		if(typeof data == 'object'){
-			Object.keys(data).forEach(k=>{
-				if(typeof this[k] != 'undefined'){
-					this[k] = data[k];
-				}
-			});
-		}
-	}
-	
-	onFromChanged(e){		
-		if(this.level_from<1)
-			this.level_from = 1;
-		if(this.level_from>70)
-			this.level_from = 70;
-		this.calc.calc();
-	}
-	
-	onToChanged(e){
-		if(this.level_to<1)
-			this.level_to = 1;
-		if(this.level_to>70)
-			this.level_to = 70;
-		this.calc.calc();
-	}
-	
-	setTypeDropDownVal(v){
-		this.type_dropdown.find('.dropdown-toggle').html(this.type_dropdown.find('[data-value="'+v+'"]').html());
-	}
-	
-	onTypeSelect(e){
-		this.type = $(e.currentTarget).data('value');
-		this.type_field.change();
-	}
-	
-	setCalc(summary){
-		this.summary_fields.forEach((f, k)=>{
-			if(f.is('.item_hours_premium, .item_hours')){
-				f.html(this.calc.formatTime(summary[k]));
-			}else{
-				f.html(summary[k]);
-			}
-		});
-	}
-	
-	get url_data(){
-		return this.type+':'+this.level_from+':'+this.level_to+':'+this.quantity;
-	}
-	
-	get quantity(){
-		return this.quantity_field.val();
-	}
-	
-	get type(){
-		return this.type_field.val();
-	}
-	
-	get level_from(){
-		return this.level_from_field.val();
-	}
-	
-	get level_to(){
-		return this.level_to_field.val();
-	}
-	
-	set url_data(v){
-		let split_data = v.split(':');
-		this.type = split_data[0];
-		this.level_from = split_data[1];
-		this.level_to = split_data[2];
-		this.quantity = split_data[3];
-	}
-	
-	set quantity(v){
-		return this.quantity_field.val(v);
-	}
-	
-	set type(v){
-		this.setTypeDropDownVal(v);
-		this.type_field.val(v);
-	}
-	
-	set level_from(v){
-		this.level_from_field.val(v);
-	}
-	
-	set level_to(v){
-		this.level_to_field.val(v);
-	}
-}
-
-class SetsCalcHelper {
-	static fallbackCopyTextToClipboard(text, cb) {
-	  var ta = document.createElement("textarea");
-	  ta.value = text;
-	  ta.style.top = "0";
-	  ta.style.left = "0";
-	  ta.style.opacity = "0";
-	  ta.style.position = "fixed";
-
-	  document.body.appendChild(ta);
-	  ta.focus();
-	  ta.select();
-
-	  try {
-		var successful = document.execCommand('copy');
-		cb(successful);
-	  } catch (err) {
-		cb(false);
-	  }
-
-	  document.body.removeChild(ta);
-	}
-	static copyTextToClipboard(text, cb) {
-	  if (!navigator.clipboard) {
-		this.fallbackCopyTextToClipboard(text, cb);
-		return;
-	  }
-	  navigator.clipboard.writeText(text).then(function() {
-		cb(true);
-	  }, function(err) {
-		cb(false);
-	  });
-	}
-}
-
 $(document).ready(function(){
-window.calcInstace = new SetsCalc();
+window.calcInstace = new ActivityCalc();
 });
